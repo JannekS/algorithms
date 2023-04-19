ae69a30ffa73cb5e5ea5dfb0da481ad8abf2afcc
Jannek <janneks@tutanota.com>
2023-04-19 23:33:43 +0200

    Added Google fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+        mono: ['Source Code Pro', 'monospace'],
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
