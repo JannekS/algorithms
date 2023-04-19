c8b60cbdf84a3893a0a4364645ffeb7b16b33ca3
Jannek <janneks@tutanota.com>
2023-04-19 23:26:37 +0200

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-96 h-screen mx-auto bg-green-400">
+    <h2 class="p-8 text-center text-2xl font-bold text-slate-700">Hello Vue World!</h2>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
