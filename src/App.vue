e6ec3493e151f41cab62ad2c293bf48017e25b3f
Jannek <janneks@tutanota.com>
2023-04-20 20:43:44 +0200

    Made header transparent on window scroll.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <the-header />
+    <the-header :headerStyle="headerStyle" />
 
-    <main>
+    <main @scroll="handleScroll">
       <router-view />
     </main>
 
@@ -15,9 +15,30 @@ import TheHeader from '@/components/TheHeader.vue';
 import TheFooter from '@/components/TheFooter.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      headerStyle: 'header-full',
+    };
+  },
   components: {
     TheHeader,
     TheFooter,
+  },
+
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 40) {
+        this.headerStyle = 'header-transparent';
+      } else {
+        this.headerStyle = 'header-full';
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
   },
 };
 </script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,22 @@
 <template>
   <header
-    class="h-14 w-full sticky top-0 left-0 z-50 header-full flex flex-row items-center transition-all duration-500"
+    class="h-14 w-full sticky top-0 left-0 z-50 flex flex-row items-center transition-all duration-500"
+    :class="headerStyle"
   >
     <img src="/icons/code-solid.svg" alt="show code" id="logo" class="w-8 h-8 mx-6 invert" />
-    <h1 class="text-xl text-white font-bold">Code Cards</h1>
+    <h1 class="text-xl text-white tracking-wider font-bold">Algorithms</h1>
   </header>
 </template>
 
 <script>
 export default {
   name: 'TheHeader',
+  props: ['headerStyle'],
+  data() {
+    return {
+      headerDesign: 'header-full',
+    };
+  },
 };
 </script>
 
