a5eaf91dc3d4cf3e3e444692019e9ca38f00d938
Jannek <janneks@tutanota.com>
2023-04-21 23:15:02 +0200

    Created BaseButton component.

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    class="flex flex-row text-xs text-black justify-center items-center p-2 space-x-2 font-mono rounded-lg bg-lime-600 m-2 hover:bg-lime-500"
+    @click="onClick"
+  >
+    <slot>
+      <div v-if="imgUrl">
+        <img :src="imgUrl" class="w-4 h-4" />
+      </div>
+      <span id="code-btn-label" class="flex">{{ value }}</span>
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    value: {
+      type: String,
+      default: 'Click here',
+    },
+    imgUrl: {
+      type: String,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: 'button',
+    },
+  },
+  emits: ['btnClicked'],
+  methods: {
+    onClick(event) {
+      this.$emit('btnClicked', event.target.name);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
