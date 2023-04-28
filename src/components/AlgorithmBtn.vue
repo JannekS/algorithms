543f91b0de5d1d1069654a0cc4f693fc3a034462
Jannek <janneks@tutanota.com>
2023-04-28 21:21:50 +0200

    Created AlgorithmBtn component.

---
+++ src/components/AlgorithmBtn.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="m-2 p-4 w-full max-w-sm rounded-lg bg-slate-600 text-white font-mono text-center text-lg hover:bg-cyan-600 hover:font-bold duration-300"
+  >
+    {{ algorithm }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AlgorithmBtn',
+  props: {
+    algorithm: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="w-96 mx-auto bg-green-400">
-    <h2 class="p-8 text-center text-2xl font-bold text-slate-700">Hello Vue World!</h2>
-  </div>
+  <algorithm-btn algorithm="bubbleSort" />
   <code-card />
 </template>
 
 <script>
 import CodeCard from '@/components/CodeCard.vue';
+import AlgorithmBtn from '../components/AlgorithmBtn.vue';
 export default {
   name: 'HomeView',
   components: {
     CodeCard,
+    AlgorithmBtn,
   },
 };
 </script>
