d847fbc8febb1696344ebf816b956931d5bc967b
Jannek <janneks@tutanota.com>
2023-05-02 19:50:06 +0200

    Click on algorithmBtn fills CodeCard with corresponding data.

--- src/components/AlgorithmBtn.vue
+++ src/components/AlgorithmBtn.vue
@@ -1,6 +1,7 @@
 <template>
   <button
-    class="m-2 p-4 w-full max-w-sm rounded-lg bg-slate-600 text-white font-mono text-center text-lg hover:bg-cyan-600 hover:font-bold duration-300"
+    @click="btnClicked"
+    class="m-2 p-4 w-52 max-w-sm rounded-lg bg-slate-600 text-white font-mono text-center text-lg hover:bg-cyan-600 hover:font-bold duration-300"
   >
     {{ algorithm }}
   </button>
@@ -13,6 +14,12 @@ export default {
     algorithm: {
       type: String,
       default: '',
+    },
+  },
+  emits: ['btnClicked'],
+  methods: {
+    btnClicked() {
+      this.$emit('btnClicked', this.algorithm);
     },
   },
 };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
-  <li v-for="algorithmObj in algorithmData" :key="algorithmObj.id">
-    <algorithm-btn :algorithm="algorithmObj.name" />
-  </li>
-  <code-card :algorithmData="algorithmData.selectionSort" />
+  <ul class="flex flex-col justify-center items-center mt-10">
+    <li v-for="algorithmObj in algorithmData" :key="algorithmObj.id">
+      <algorithm-btn :algorithm="algorithmObj.name" @btnClicked="openCodeCard" />
+    </li>
+  </ul>
+
+  <code-card :algorithmData="algorithmData[selectedAlgorithm]" />
 </template>
 
 <script>
@@ -32,71 +35,16 @@ export default {
   data() {
     return {
       algorithmData: {},
-      // algorithms: {
-      //   bubbleSort: {
-      //     name: 'bubbleSort',
-      //     description:
-      //       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem obcaecati ipsa laudantium pariatur amet illo repellendus neque error quisquam, porro mollitia quae est placeat, consequuntur aliquid ex ipsum exercitationem aspernatur! Velsaepe molestias facere, distinctio sint cum reprehenderit omnis nobis eveniet explicabo voluptatem magnam quae id placeat tenetur ad ratione fuga soluta excepturi quo. Cupiditate, commodi iusto. Atque, neque aspernatur.',
-      //     complexity: 'n^2',
-      //     codeString:
-      //       '\nbubbleSort(arr) {\n\tfor (let i = arr.length; i > 0; i--) {\n\t\tfor (var j = 0; j < i - 1; j++) {\n\t\t\tif (arr[j] > arr[j + 1]) {\n\t\t\t\tlet temp = arr[j];\n\t\t\t\tarr[j] = arr[j + 1];\n\t\t\t\tarr[j + 1] = temp;\n\t\t\t}\n\t\t}\n\t}\n}',
-      //     async runCode(arr, colorMarkers) {
-      //       for (let i = arr.length; i > 0; i--) {
-      //         for (let j = 0; j < i - 1; j++) {
-      //           if (this.reset) {
-      //             return;
-      //           }
-      //           colorMarkers.sortedElement = i;
-      //           colorMarkers.selectedElement = j;
-      //           if (arr[j] > arr[j + 1]) {
-      //             let temp = arr[j];
-      //             arr[j] = arr[j + 1];
-      //             arr[j + 1] = temp;
-      //             colorMarkers.selectedElement = j + 1;
-      //           }
-      //           await this.delay(300);
-      //         }
-      //       }
-      //       colorMarkers.sortedElement = 0;
-      //     },
-      //   },
-      //   selectionSort: {
-      //     name: 'selectionSort',
-      //     description:
-      //       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem obcaecati ipsa laudantium pariatur amet illo repellendus neque error quisquam, porro mollitia quae est placeat, consequuntur aliquid ex ipsum exercitationem aspernatur! Velsaepe molestias facere, distinctio sint cum reprehenderit omnis nobis eveniet explicabo voluptatem magnam quae id placeat tenetur ad ratione fuga soluta excepturi quo. Cupiditate, commodi iusto. Atque, neque aspernatur.',
-      //     complexity: 'n^2',
-      //     codeString:
-      //       'selectionSort(arr) {\nfor (let i = 0; i < arr.length; i++) {\n\tlet minIndex = i;\n\tfor (let j = i + 1; j < arr.length; j++) {\n\t\tif (arr[j] < arr[minIndex]) {\n\t\t\tminIndex = j;\n\t\t}\n\t}\n\tif (minIndex !== i) {\n\t\tlet temp = arr[i];\n\t\tarr[i] = arr[minIndex];\n\t\tarr[minIndex] = temp;\n\t}\n}',
-      //     async runCode(arr, colorMarkers) {
-      //       for (let i = 0; i < arr.length; i++) {
-      //         let minIndex = i;
-      //         colorMarkers.minElement = i;
-      //         colorMarkers.swapElement = i;
-      //         for (let j = i + 1; j < arr.length; j++) {
-      //           if (this.reset) {
-      //             return;
-      //           }
-      //           colorMarkers.selectedElement = j;
-      //           if (arr[j] < arr[minIndex]) {
-      //             minIndex = j;
-      //             colorMarkers.minElement = j;
-      //           }
-      //           await this.delay(300);
-      //         }
-      //         if (minIndex !== i) {
-      //           let temp = arr[i];
-      //           arr[i] = arr[minIndex];
-      //           arr[minIndex] = temp;
-      //         }
-      //         colorMarkers.minElement = i;
-      //         colorMarkers.swapElement = minIndex;
-      //         await this.delay(500);
-      //         colorMarkers.sortedElement = i;
-      //       }
-      //     },
-      //   },
-      // },
+      selectedAlgorithm: {
+        type: String,
+        default: '',
+      },
     };
+  },
+  methods: {
+    openCodeCard(algorithm) {
+      this.selectedAlgorithm = algorithm;
+    },
   },
 };
 </script>
