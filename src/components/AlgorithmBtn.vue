45c94bdc9b73c00119e2bf14f2a52f40cb35a42a
Jannek <janneks@tutanota.com>
2023-05-04 23:16:48 +0200

    Improved styling and behaviour of AlgorithmBtn.

--- src/components/AlgorithmBtn.vue
+++ src/components/AlgorithmBtn.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="btnClicked"
-    class="m-2 p-4 w-52 max-w-sm rounded-lg bg-slate-600 text-white font-mono text-center text-lg hover:bg-cyan-600 hover:font-bold duration-300"
+    class="m-2 p-4 w-52 max-w-sm rounded-lg bg-gradient-to-bl from-slate-400 to-slate-500 shadow-md text-white font-mono text-center text-lg hover:from-cyan-400 hover:to-cyan-600 hover:scale-105 hover:shadow-2xl hover:font-bold active:from-cyan-600 active:to-cyan-600 active:scale-100 active:shadow-sm duration-300"
   >
     {{ algorithm }}
   </button>
